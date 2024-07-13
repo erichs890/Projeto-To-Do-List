5de1f1061e3dce5ff3ac300412a2dc62ebfd5265
Erich Schlaepfer <114402221+erichs890@users.noreply.github.com>
2024-07-13 00:41:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ const divTarefas = document.querySelector('#tarefas')
 botaoAdd.addEventListener('click',()=>{
     const tarefas = document.querySelector("#addInput").value
     const checkbox = document.createElement('input')
-    if(!tarefas){
-        alert("Preencha o campo das tarefas")
-    }else{
     checkbox.type = 'checkbox'
     const li = document.createElement('li')
     li.textContent = tarefas
-    }
+    
     checkbox.addEventListener('change', ()=>{
         if(checkbox.checked){
             li.style.textDecoration = "line-through"
